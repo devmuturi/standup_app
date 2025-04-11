e9e00a2a064671faa8baf9dca9edd3d93dd7c91a
devmuturi <muturinjonge09@gmail.com>
2025-04-11 16:32:55 +0300

    Setup a dropdown stimulus controller manually

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,9 @@
 import { Application } from "@hotwired/stimulus"
+import Dropdown from "@stimulus-components/dropdown"
 
 const application = Application.start()
+application.register('dropdown', Dropdown)
+
 
 // Configure Stimulus development experience
 application.debug = false
