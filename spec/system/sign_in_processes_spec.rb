55f4c51ff43cffb9f1e1c26b7ba0fe7498c6a012
devmuturi <muturinjonge09@gmail.com>
2025-04-12 02:59:20 +0300

    Test the sign up page

--- spec/system/sign_in_processes_spec.rb
+++ spec/system/sign_in_processes_spec.rb
@@ -5,37 +5,36 @@ RSpec.describe "SignInProcesses", type: :system do
     driven_by(:rack_test)
   end
 
-  let(:password) { "password" }
+  let(:password) { '123456789' }
+
   let(:user) do
     FactoryBot.create(:user, {
       password: password,
       password_confirmation: password
     })
+  end
 
-    it "should require the user to log in and successfully logs in" do
-      visit root_path
+  it "should require the user to log in and successfully log in" do
+    visit root_path
 
-      within "#new_user" do
-        fill_in "user_email", with: user.email
-        fill_in "user_password", with: password
-      end
-
-      click_button "Log in"
-
-      expect(current_path).to eql(root_path)
+    within "#new_user" do
+      fill_in "user_email", with: user.email
+      fill_in "user_password", with: password
     end
 
-    it "should fail on an invalid user" do
-      visit root_path
+    click_button "Log in"
+    expect(current_path).to eql(root_path)
+  end
 
-      within "#new_user" do
-        fill_in "user_email", with: "test"
-        fill_in "user_password", with: "password"
-      end
+  it "should fail on an invalid user" do
+    visit root_path
 
-      click_button "Log in"
-
-      expect(current_path).to eql(new_user_session_path)
+    within "#new_user" do
+      fill_in "user_email", with: 'test'
+      fill_in "user_password", with: 'password'
     end
+
+    click_button "Log in"
+    expect(current_path).to eql(new_user_session_path)
   end
 end
