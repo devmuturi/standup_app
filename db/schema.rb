2e5d5a2a7a60c4bbcafe1debb1361abf8c0575ed
devmuturi <muturinjonge09@gmail.com>
2025-04-11 17:55:57 +0300

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_11_143533) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_11_145529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
 
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "time_zone"
+  end
 end
