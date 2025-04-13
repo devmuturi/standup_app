f67d43e81ab2bdb36404a323c2112a76b8635f68
devmuturi <muturinjonge09@gmail.com>
2025-04-13 11:21:23 +0300

    create a helper method for account(current_account)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
-class ApplicationController < ActionController::Base 
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :authenticate_user!
   layout :layout_by_resource
+
+  helper_method :current_account
+
+  def current_account
+    @current_account ||= current_user.account
+    @current_account
+  end
 
   protected
 
